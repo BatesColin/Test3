e949ed99a5623f2b8197b1257f6aa151e4ba160a
cb <dragonflylord@gmail.com>
2018-06-08 11:56:33 -0700

    It will now not conect to index

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,20 @@ $(function(){
   var replacement3 = "0";
   var replacements = ("3", "1", "0", "2", "4", "5", "6", "7", "8", "9");
   //var replacement4 = "2", "4", "5", "6", "7", "8", "9"
-  var input = parseInt($("#input").val().trim());
+  var input = parseInt($("#input").val());
+
+ var inputSplit = input.toString().split("");
 
 
-  console.log(input)
 
-//function tempName(SplitInput)
-  //for(var index = 0; splitInput<9; index++){
+  for(var index = 0; inputSplit<9; index++){
   //  var value = replacements [index];
- if (splitInput=== "replacements".indexOf["3"]){
+ if (inputSplit === "replacements".indexOf["0"]){
       //colsole.log(3)
-  // }
+      console.log(input)
+   }
 //  colsole.log()
-// }
+ }
 
 });
 
