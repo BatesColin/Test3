439bcdba5f4e81635d7255db791651d169f4f00d
cb <dragonflylord@gmail.com>
2018-06-08 16:57:06 -0700

    It will now show my restarted JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,37 @@
 $(function(){
   $("#form-one").submit(function(event){
   event.preventDefault();
-  var replacement1 = "3";
-  var replacement2 = "1";
-  var replacement3 = "0";
-  var replacements = ("3", "1", "0", "2", "4", "5", "6", "7", "8", "9");
-  //var replacement4 = "2", "4", "5", "6", "7", "8", "9"
-  var input = parseInt($("#input").val());
+  var input = parseInt($("input#input").val());
+  var inputLength =input.inputLength;
+  console.log("input")
+  var outPut = []
+  var inputClone = inputNumber
+  var number1.indexof[input]
+  console.log(number1)
 
- var inputSplit = input.toString().split("");
+  //var inputNumber = concat.input
+  for(inputNumber=inputNumber+1; inputNumber>0; inputNumber--){
+    console.log(inputNumber)
+    var outPut1 =toString(inputClone-1)
+    if(outPut1.includes(3){
+      outPut.replace(outPut,"I'm sorry, Dave. I'm afraid I can't do that.")
 
+    }elseif(outPut1.includes(1){
+      outPut.replace(outPut,"Boop!")
 
+    }elseif(output1.includes(0){
+      outPut.replace(outPut, "Beep!")
+    }
 
-  for(var index = 0; inputSplit<9; index++){
-  //  var value = replacements [index];
- if (inputSplit === "replacements".indexOf["0"]){
-      //colsole.log(3)
-      console.log(input)
-   }
-//  colsole.log()
- }
+    }
 
+  }
+  //for(index<inputLength; var input=0: index++){
+  //for(var index = input; index <= inputLength; index++){
+});
 });
 
-});
+
+
+// var output = input.map(input(input) {
+// outPut.push(number-1);
---
+++ js/scripts1.js
@@ -0,0 +1,49 @@
+$(function(){
+  $("#form-one").submit(function(event){
+  event.preventDefault();
+  var replacement1 = "3";
+  var replacement2 = "1";
+  var replacement3 = "0";
+  var replacements = ("3", "1", "0", "2", "4", "5", "6", "7", "8", "9");
+  var replaceWith = ("I'm sorry, Dave. I'm afraid I can't do that.", "Boop!", "Beep!" )
+  //var replacement4 = "2", "4", "5", "6", "7", "8", "9"
+  var input = parseInt($("input#input").val());
+  var inputLength =input.inputLength;
+
+  var inputSplit = input.toString().split("");
+
+ //.includes[inputSplit]{
+
+
+ inputSplit.foreach(function(input)
+  //for(var index = inputSplit; index <= inputLength; index++)
+  {
+    // var value = inputSplit[index]
+    // var temp = input-value
+  if(replacements.includes[inputSplit(0)]){
+    var output = inputSplit.indexof(replacements)
+
+
+     console.log('output')
+     //if(){
+
+
+    // if(inputSplit =replacement1){
+    //   alert("yes")
+    //   var output = "I'm sorry, Dave. I'm afraid I can't do that."
+    // }
+    // elseif(inputSplit = replacement2)
+    //  alert("maybe")
+    //
+
+   //var value = replacements [index];
+  //if (.includes[inputSplit]{
+
+
+    //if (input === 3){
+  alert(temp);
+
+      //colsole.log(3)
+      console.log(temp)
+
+//  colsole.log()
