cfe05fbca14b17e66c37192d8154d356d5bd31d7
cb <dragonflylord@gmail.com>
2018-06-08 11:34:55 -0700

    It will now have a few functions in it.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(function(){
+  $("#form-one").submit(function(event){
+  event.preventDefault();
+  var replacement1 = "3";
+  var replacement2 = "1";
+  var replacement3 = "0";
+  var replacements = ("3", "1", "0", "2", "4", "5", "6", "7", "8", "9");
+  //var replacement4 = "2", "4", "5", "6", "7", "8", "9"
+  var input = parseInt($("#input").val().trim());
+
+
+  console.log(input)
+
+//function tempName(SplitInput)
+  //for(var index = 0; splitInput<9; index++){
+  //  var value = replacements [index];
+ if (splitInput=== "replacements".indexOf["3"]){
+      //colsole.log(3)
+  // }
+//  colsole.log()
+// }
+
+});
+
+});
