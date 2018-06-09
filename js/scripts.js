7394619bfd7517c1eeb59dea57596cc0c7eadba0
cb <dragonflylord@gmail.com>
2018-06-08 22:39:28 -0700

    It will now display the requisits for the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,39 @@
 $(function(){
   $("#form-one").submit(function(event){
   event.preventDefault();
+
+
+
+
+
   var input = parseInt($("input#input").val());
-  var inputLength =input.inputLength;
-  console.log("input")
+
+  console.log(input)
   var outPut = []
-  var inputClone = inputNumber
-  var number1.indexof[input]
-  console.log(number1)
+
+
 
   //var inputNumber = concat.input
-  for(inputNumber=inputNumber+1; inputNumber>0; inputNumber--){
-    console.log(inputNumber)
-    var outPut1 =toString(inputClone-1)
-    if(outPut1.includes(3){
-      outPut.replace(outPut,"I'm sorry, Dave. I'm afraid I can't do that.")
+  for(var i =0; i<=input; i++){
+    console.log(i);
+    var temp = i.toString();
+    if(i % 3 === 0){
+      outPut.push("I'm sorry, Dave. I'm afraid I can't do that.");
 
-    }elseif(outPut1.includes(1){
-      outPut.replace(outPut,"Boop!")
+    }else if(temp.includes(1)){
+      outPut.push("Boop!");
 
-    }elseif(output1.includes(0){
-      outPut.replace(outPut, "Beep!")
+    }else if(temp.includes(0)){
+      outPut.push("Beep!");
     }
-
+    else{
+      outPut.push(i);
     }
-
   }
+
+  for(i=0; i<outPut.length; i++){
+  $("#list").append("<li>" + outPut[i] + "</li>");
+}
   //for(index<inputLength; var input=0: index++){
   //for(var index = input; index <= inputLength; index++){
 });
